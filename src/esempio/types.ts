7492da1837eef12d5ce480dd2c839d5f994c45ea
Enricove <enricovegliani@gmail.com>
2024-02-09 11:25:46 +0100

    test newOrder

---
+++ src/esempio/types.ts
@@ -0,0 +1,16 @@
+export type UserType = {
+    name: string
+    surname: string
+    codiceFiscale: string
+    eMail: string
+}
+
+export type OrderType = {
+    user: UserType
+    creationDate: Date;
+    updateDate: Date;
+    giftCard10: number
+    giftCard20: number
+    giftCard50: number
+    giftCard100: number
+}
---
+++ src/utils.test.ts
@@ -0,0 +1,88 @@
+import { OrderType, UserType } from "./esempio/types";
+
+describe("testcase giftcard shop", () =>{
+    
+    
+    it("create new order", () =>{
+        //pass user oject and gets an order object
+        const user: UserType = {
+            name: "enrico",
+            surname: "vegliani",
+            codiceFiscale: "abcabc12a01f205a",
+            eMail: "mail@example.com"
+        }
+        const order: OrderType = newOrder(user);
+        expect(order.user).toBe(user);
+    })
+
+    it("create order with no name", () =>{
+        //pass wrong user and expect error
+        const user: UserType = {
+            name: "",
+            surname: "vegliani",
+            codiceFiscale: "abcabc12a01f205a",
+            eMail: "mail@example.com"
+        }
+        
+        expect(newOrder(user)).toThrow("invalid name");
+    })
+
+    it("create order with no ", () =>{
+        //pass wrong user and expect error
+        const user: UserType = {
+            name: "enrico",
+            surname: "",
+            codiceFiscale: "abcabc12a01f205a",
+            eMail: "mail@example.com"
+        }
+        expect(newOrder(user)).toThrow("invalid surname");
+    })
+
+    it("create order with no codice fiscale", () =>{
+        //pass wrong user and expect error
+        const user: UserType = {
+            name: "enrico",
+            surname: "vegliani",
+            codiceFiscale: "",
+            eMail: "mail@example.com"
+        }
+        expect(newOrder(user)).toThrow("invalid codice fiscale");
+    })
+
+    it("create order with no email", () =>{
+        //pass wrong user and expect error
+        const user: UserType = {
+            name: "enrico",
+            surname: "vegliani",
+            codiceFiscale: "abcabc12a01f205a",
+            eMail: ""
+        }
+        expect(newOrder(user)).toThrow("invalid email");
+    })
+
+    it("create order with wrong email", () =>{
+        //pass wrong email and expect error
+        const user: UserType = {
+            name: "enrico",
+            surname: "vegliani",
+            codiceFiscale: "abcabc12a01f205a",
+            eMail: "mailsbagliata.com"
+        }
+        expect(newOrder(user)).toThrow("invalid email");
+     })
+
+    it("create order with wrong codice fiscale", () =>{
+       //pass wrong codice fiscale and expect error
+       const user: UserType = {
+        name: "enrico",
+        surname: "vegliani",
+        codiceFiscale: "codice fiscale errato",
+        eMail: "mail@example.com"
+        }
+        expect(newOrder(user)).toThrow("invalid codice fiscale");
+    })
+
+
+
+    
+})
